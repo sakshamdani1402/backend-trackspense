371d4ea57033ea4fbf1d17a9fde07f02d0da7395
sakshamdani1402 <sakshamdsni@gmail.com>
2022-08-10 18:43:47 +0530

    procfile added

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const express = require('express')
 const app = express();
 connnectToMongo();
 const port = process.env.PORT||5000;
-
 app.use(express.json());
 app.use(cors());
 //Available routes
 app.use('/auth',require('./routes/auth'));
 app.use('/transactions',require('./routes/transactions'));
 app.use('/saved', require('./routes/saved'));
+app.get('/', (req,res) =>{
+    res.send('app is running');
+})
 app.listen(port, () => {
     console.log(`iNotebook  backend listening at http://localhost:${port}`)
 })
